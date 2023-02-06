0c7b8d1af2fc80f3c38c84496792c9c1159b7727
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-02-06 15:21:45 +0000

    add featured speakers dynamically in with js file

--- js/main.js
+++ js/main.js
@@ -4,31 +4,75 @@ const featureGuest = {
     guestOne: {
         name: 'First Person',
         skills: 'Contre-bass',
-        nationality: 'USA'
+        nationality: 'USA',
+        picture: "/img/iconmonstr-school-6-240.png"
     },
     guestTwo: {
         name: 'Second Person',
         skills: 'keyboard',
-        nationality: 'Germmany'
+        nationality: 'Germmany',
+        picture: "/img/iconmonstr-school-6-240.png"
     },
     guestThree: {
         name: 'third Person',
         skills: 'drums',
-        nationality: 'France'
+        nationality: 'France',
+        picture: "/img/iconmonstr-school-6-240.png"
     },
     guestFour: {
         name: 'fourth Person',
         skills: 'Singer',
-        nationality: 'Maroc'
+        nationality: 'Maroc',
+        picture: "/img/iconmonstr-school-6-240.png"
     },
     guestFive: {
         name: 'fifth Person',
         skills: 'Guitare',
-        nationality: 'Congo'
+        nationality: 'Congo',
+        picture: "/img/iconmonstr-school-6-240.png"
     },
     guestSix: {
         name: 'sixth Person',
         skills: 'Bass',
-        nationality: 'Togo'
+        nationality: 'Togo',
+        picture: "/img/iconmonstr-school-6-240.png"
     }
+}
+
+const featureSpeakersSection = document.querySelector('.featured-speakers');
+
+for (let guest in featureGuest) {
+
+    console.log(featureGuest[guest].name)
+
+    const everyFeatureDiv = document.createElement('div');
+    everyFeatureDiv.className = 'every-Feature';
+
+    const speakerPicture = document.createElement('img');
+    speakerPicture.className = 'speaker-picture';
+    
+    
+    const speakerDescription = document.createElement('div');
+    speakerDescription.className = 'speaker-description';
+
+    const speakerName = document.createElement('h4');
+    speakerName.className = 'speaker-name';
+    const speakerSkills = document.createElement('p');
+    speakerSkills.className = 'speaker-skills';
+    const speakerNationality = document.createElement('p');
+    speakerNationality.className = 'speaker-nationality';
+
+    speakerPicture.src = featureGuest[guest].picture;
+    speakerPicture.alt = featureGuest[guest].name +' picture';
+    speakerName.innerText = featureGuest[guest].name;
+    speakerSkills.innerText = featureGuest[guest].skills;
+    speakerNationality.innerText = featureGuest[guest].nationality;
+    
+    speakerDescription.appendChild(speakerName);
+    speakerDescription.appendChild(speakerSkills);
+    speakerDescription.appendChild(speakerNationality);
+
+    everyFeatureDiv.appendChild(speakerDescription);
+    everyFeatureDiv.appendChild(speakerPicture);
+    featureSpeakersSection.appendChild(everyFeatureDiv);
 }
