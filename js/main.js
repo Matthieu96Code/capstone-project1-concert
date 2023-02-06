eea3485ed498a257b5a81570e4fde4612243cf81
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-02-06 14:03:35 +0000

    add main.js file add an object with draft data for speaker in js file

---
+++ js/main.js
@@ -0,0 +1,34 @@
+/* object for feature guest */
+
+const featureGuest = {
+    guestOne: {
+        name: 'First Person',
+        skills: 'Contre-bass',
+        nationality: 'USA'
+    },
+    guestTwo: {
+        name: 'Second Person',
+        skills: 'keyboard',
+        nationality: 'Germmany'
+    },
+    guestThree: {
+        name: 'third Person',
+        skills: 'drums',
+        nationality: 'France'
+    },
+    guestFour: {
+        name: 'fourth Person',
+        skills: 'Singer',
+        nationality: 'Maroc'
+    },
+    guestFive: {
+        name: 'fifth Person',
+        skills: 'Guitare',
+        nationality: 'Congo'
+    },
+    guestSix: {
+        name: 'sixth Person',
+        skills: 'Bass',
+        nationality: 'Togo'
+    }
+}
