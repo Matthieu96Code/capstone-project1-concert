30ebaf8bdb364f329f0e1836505267241affd24e
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-02-07 15:34:14 +0000

    add more button to speakers section and dynamic function

---
+++ js/aboutme.js
@@ -0,0 +1,10 @@
+/* Add the header to about me page */
+
+const headerLink = document.querySelector('.main-program');
+const aboutmeHeader = document.querySelector('#aboutme-header');
+// aboutmeHeader.append(headerLink)
+console.log(headerLink);
+console.log(aboutmeHeader);
+console.log(.document.querySelector('.last-festival'));
+/* object for feature guest */
+
--- js/main.js
+++ js/main.js
@@ -1,48 +1,61 @@
-/* object for feature guest */
+console.log(document.querySelector('.last-festival'))
 
 const featureGuest = {
     guestOne: {
         name: 'Luc Mayitoukoua',
         skills: 'Contre-bass',
         nationality: 'Senegal',
-        picture: "/img/luc.png"
+        picture: "./img/luc.png"
     },
     guestTwo: {
         name: 'Stéphanie Soleansky',
         skills: 'keyboard',
         nationality: 'France',
-        picture: "/img/steph.png"
+        picture: "./img/steph.png"
     },
     guestThree: {
         name: 'Natalie Aloessode Bernardin',
         skills: 'drums',
         nationality: 'Canada',
-        picture: "/img/natalie.png"
+        picture: "./img/natalie.png"
     },
     guestFour: {
         name: 'Mawuto Dick',
         skills: 'Singer',
         nationality: 'Maroc',
-        picture: "/img/mawuto.png"
+        picture: "./img/mawuto.png"
     },
     guestFive: {
         name: 'Cyriaque Noussouglo',
         skills: 'Artiste et co-fondatrice Afropolitain Nomade',
         nationality: 'Canada',
-        picture: "/img/cyriaque.png"
+        picture: "./img/cyriaque.png"
     },
     guestSix: {
         name: 'Vanessa Kanga',
         skills: 'Artiste et co-fondatrice Afropolitain Nomade',
         nationality: 'Togo',
-        picture: "/img/vanessa.png"
+        picture: "./img/vanessa.png"
     }
 }
 
 const featureSpeakersSection = document.querySelector('.featured-speakers');
 
-for (let guest in featureGuest) {
+const SpeakersSection = document.querySelector('.speakers-section');
 
+const featuredSpeakersTitle = document.createElement('h3');
+featuredSpeakersTitle.className = 'lato-font-black';
+featuredSpeakersTitle.innerHTML = 'Featured Speakers<span class="underline-style"></span>'
+SpeakersSection.append(featuredSpeakersTitle);
+
+const moreSpeakers = document.createElement('button');
+moreSpeakers.className = 'fa fa-chevron-down';
+moreSpeakers.innerHTML = 'More'
+
+SpeakersSection.append(moreSpeakers);
+
+
+function callSpeakers (guest) {
     const everyFeatureDiv = document.createElement('div');
     everyFeatureDiv.className = 'every-feature';
 
@@ -60,6 +73,7 @@ for (let guest in featureGuest) {
     const speakerNationality = document.createElement('p');
     speakerNationality.className = 'speaker-nationality';
 
+
     speakerPicture.src = featureGuest[guest].picture;
     speakerPicture.alt = featureGuest[guest].name +' picture';
     speakerName.innerText = featureGuest[guest].name;
@@ -75,3 +89,36 @@ for (let guest in featureGuest) {
     featureSpeakersSection.appendChild(everyFeatureDiv);
 }
 
+
+let stObjCount = 0;
+
+for (let guest in featureGuest) {
+    stObjCount +=1 ;
+    
+    if (stObjCount < 3) {
+        callSpeakers(guest)  
+    } 
+    
+}
+
+
+function ndSpeakers() {
+    let ndObjCount = 0;
+    for (let guest in featureGuest) {
+        ndObjCount +=1 ;
+        
+        if (ndObjCount > 2) {
+            callSpeakers(guest)
+        }  
+    }
+}
+
+const x = window.matchMedia('(min-width: 768px)');
+if (x.matches) {
+    ndSpeakers()
+}
+const moreSpeakersBtn = document.querySelector('.speakers-section button')
+moreSpeakersBtn.addEventListener('click', ndSpeakers);
+moreSpeakersBtn.addEventListener('click', () =>{
+    moreSpeakersBtn.remove()
+});
