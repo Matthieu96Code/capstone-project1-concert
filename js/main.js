8af3e9929e66ffae2a6f0a10665e721ed6011c6b
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-02-08 19:36:00 +0000

    install eslint

--- js/main.js
+++ js/main.js
@@ -1,42 +1,41 @@
-
 const featureGuest = {
-    guestOne: {
-        name: 'Luc Mayitoukoua',
-        job: 'Cultural entrepreneur, consultant and director ZHU Culture from Senegal',
-        subject: 'Markets and cultural networks',
-        picture: "./img/luc.png"
-    },
-    guestTwo: {
-        name: 'Stéphanie Soleansky',
-        job: 'Consultant and cultural entrepreneur from France',
-        subject: 'Women and cultural entrepreneurship',
-        picture: "./img/steph.png"
-    },
-    guestThree: {
-        name: 'Natalie Aloessode Bernardin',
-        job: 'Consultant and artist manager from Canade',
-        subject: 'Women and cultural entrepreneurship',
-        picture: "./img/natalie.png"
-    },
-    guestFour: {
-        name: 'Mawuto Dick',
-        job: 'cultural entrepreneur, director of the togoville jazz festival from Togo',
-        subject: 'The music charters of the world',
-        picture: "./img/mawuto.png"
-    },
-    guestFive: {
-        name: 'Cyriaque Noussouglo',
-        job: 'general secretary of the national commission of francophonie in togo',
-        subject: 'cultural and creative industries',
-        picture: "./img/cyriaque.png"
-    },
-    guestSix: {
-        name: 'Vanessa Kanga',
-        job: 'Artist and co-founder of Afropolitain Nomade from Canada',
-        subject: 'women and cultural entrepreneurship',
-        picture: "./img/vanessa.png"
-    }
-}
+  guestOne: {
+    name: 'Luc Mayitoukoua',
+    job: 'Cultural entrepreneur, consultant and director ZHU Culture from Senegal',
+    subject: 'Markets and cultural networks',
+    picture: './img/luc.png',
+  },
+  guestTwo: {
+    name: 'Stéphanie Soleansky',
+    job: 'Consultant and cultural entrepreneur from France',
+    subject: 'Women and cultural entrepreneurship',
+    picture: './img/steph.png',
+  },
+  guestThree: {
+    name: 'Natalie Aloessode Bernardin',
+    job: 'Consultant and artist manager from Canade',
+    subject: 'Women and cultural entrepreneurship',
+    picture: './img/natalie.png',
+  },
+  guestFour: {
+    name: 'Mawuto Dick',
+    job: 'cultural entrepreneur, director of the togoville jazz festival from Togo',
+    subject: 'The music charters of the world',
+    picture: './img/mawuto.png',
+  },
+  guestFive: {
+    name: 'Cyriaque Noussouglo',
+    job: 'general secretary of the national commission of francophonie in togo',
+    subject: 'cultural and creative industries',
+    picture: './img/cyriaque.png',
+  },
+  guestSix: {
+    name: 'Vanessa Kanga',
+    job: 'Artist and co-founder of Afropolitain Nomade from Canada',
+    subject: 'women and cultural entrepreneurship',
+    picture: './img/vanessa.png',
+  },
+};
 
 const featureSpeakersSection = document.querySelector('.featured-speakers');
 
@@ -44,87 +43,79 @@ const SpeakersSection = document.querySelector('.speakers-section');
 
 const featuredSpeakersTitle = document.createElement('h3');
 featuredSpeakersTitle.className = 'lato-font-black';
-featuredSpeakersTitle.innerHTML = 'Featured Speakers<span class="underline-style"></span>'
+featuredSpeakersTitle.innerHTML = 'Featured Speakers<span class="underline-style"></span>';
 SpeakersSection.append(featuredSpeakersTitle);
 
 const moreSpeakers = document.createElement('button');
 moreSpeakers.className = 'lato-font-black';
-moreSpeakers.innerHTML = 'More  ▼'
+moreSpeakers.innerHTML = 'More  ▼';
 
 SpeakersSection.append(moreSpeakers);
 
+function callSpeakers(guest) {
+  const everyFeatureDiv = document.createElement('div');
+  everyFeatureDiv.className = 'every-feature';
 
-function callSpeakers (guest) {
-    const everyFeatureDiv = document.createElement('div');
-    everyFeatureDiv.className = 'every-feature';
+  const speakerPicture = document.createElement('img');
+  speakerPicture.className = 'speaker-picture';
 
-    const speakerPicture = document.createElement('img');
-    speakerPicture.className = 'speaker-picture';
-    
-    
-    const speakerDescription = document.createElement('div');
-    speakerDescription.className = 'lato-font-black';
-    speakerDescription.id = 'speaker-description';
+  const speakerDescription = document.createElement('div');
+  speakerDescription.className = 'lato-font-black';
+  speakerDescription.id = 'speaker-description';
 
-    const speakerName = document.createElement('h4');
-    speakerName.id = 'speaker-name';
+  const speakerName = document.createElement('h4');
+  speakerName.id = 'speaker-name';
 
-    const speakerSkills = document.createElement('p');
-    speakerSkills.className = 'lato-font-red';
-    speakerSkills.id = 'speaker-skills';
+  const speakerSkills = document.createElement('p');
+  speakerSkills.className = 'lato-font-red';
+  speakerSkills.id = 'speaker-skills';
 
-    const speakerNationality = document.createElement('p');
-    speakerNationality.className = 'speaker-nationality';
+  const speakerNationality = document.createElement('p');
+  speakerNationality.className = 'speaker-nationality';
 
+  speakerPicture.src = featureGuest[guest].picture;
+  speakerPicture.alt = `${featureGuest[guest].name} picture`;
+  speakerName.innerText = featureGuest[guest].name;
+  speakerSkills.innerText = featureGuest[guest].job;
+  speakerNationality.innerText = featureGuest[guest].subject;
 
-    speakerPicture.src = featureGuest[guest].picture;
-    speakerPicture.alt = featureGuest[guest].name +' picture';
-    speakerName.innerText = featureGuest[guest].name;
-    speakerSkills.innerText = featureGuest[guest].job;
-    speakerNationality.innerText = featureGuest[guest].subject;
-    
-    speakerDescription.appendChild(speakerName);
-    speakerDescription.appendChild(speakerSkills);
-    speakerDescription.appendChild(speakerNationality);
+  speakerDescription.appendChild(speakerName);
+  speakerDescription.appendChild(speakerSkills);
+  speakerDescription.appendChild(speakerNationality);
 
-    everyFeatureDiv.appendChild(speakerDescription);
-    everyFeatureDiv.appendChild(speakerPicture);
-    featureSpeakersSection.appendChild(everyFeatureDiv);
+  everyFeatureDiv.appendChild(speakerDescription);
+  everyFeatureDiv.appendChild(speakerPicture);
+  featureSpeakersSection.appendChild(everyFeatureDiv);
 }
-
 
 let stObjCount = 0;
 
-for (let guest in featureGuest) {
-    stObjCount +=1 ;
-    
-    if (stObjCount < 3) {
-        callSpeakers(guest)  
-    } 
-    
+for (const guest in featureGuest) {
+  stObjCount += 1;
+
+  if (stObjCount < 3) {
+    callSpeakers(guest);
+  }
 }
 
-
 function ndSpeakers() {
-    let ndObjCount = 0;
-    for (let guest in featureGuest) {
-        ndObjCount +=1 ;
-        
-        if (ndObjCount > 2) {
-            callSpeakers(guest)
-        }  
+  let ndObjCount = 0;
+  for (const guest in featureGuest) {
+    ndObjCount += 1;
+
+    if (ndObjCount > 2) {
+      callSpeakers(guest);
     }
+  }
 }
 
 const x = window.matchMedia('(min-width: 768px)');
 if (x.matches) {
-    ndSpeakers()
+  ndSpeakers();
 }
-const moreSpeakersBtn = document.querySelector('.speakers-section button')
+const moreSpeakersBtn = document.querySelector('.speakers-section button');
 
 moreSpeakersBtn.addEventListener('click', ndSpeakers);
-moreSpeakersBtn.addEventListener('click', () =>{
-    moreSpeakersBtn.remove()
+moreSpeakersBtn.addEventListener('click', () => {
+  moreSpeakersBtn.remove();
 });
-
-
--- js/menu.js
+++ js/menu.js
@@ -1,29 +1,25 @@
+const hamburgerBtn = document.querySelector('.hamburger-class a');
 
-const hamburgerBtn = document.querySelector('.hamburger-class a')
+function callMenu() {
+  const headerSection = document.querySelector('header');
 
-function callMenu () {
+  const mainMenu = document.createElement('div');
+  mainMenu.className = 'main-menu';
 
-    const headerSection = document.querySelector('header')
+  mainMenu.innerHTML = '<a class="menu-toggle" id="close-btn" href="#" ><img src="./img/png-transparent-x-cross-wrong-sign.png" alt="close button"></a><ul><li><a href="./index.html">Home Page</a></li><li><a href="./aboutme.html">about</a></li><li><a href="./index.html#program">Program</a></li><li><a href="#">Join</a></li><li><a href="#sponsor">Sponsor</a></li><li><a href="#">News</a></li><li><a href="lp-compaign">LP Compaign</a></li></ul>';
 
-    const mainMenu = document.createElement('div');
-    mainMenu.className = 'main-menu';
-
-
-    mainMenu.innerHTML = '<a class="menu-toggle" id="close-btn" href="#" ><img src="./img/png-transparent-x-cross-wrong-sign.png" alt="close button"></a><ul><li><a href="./index.html">Home Page</a></li><li><a href="./aboutme.html">about</a></li><li><a href="./index.html#program">Program</a></li><li><a href="#">Join</a></li><li><a href="#sponsor">Sponsor</a></li><li><a href="#">News</a></li><li><a href="lp-compaign">LP Compaign</a></li></ul>'
-
-    headerSection.appendChild(mainMenu)
+  headerSection.appendChild(mainMenu);
 }
 
-hamburgerBtn.addEventListener('click', () =>{
-    callMenu ()
-    const mainMenuList = document.querySelector('.main-menu');
-    
-    const menuList = document.querySelectorAll('.main-menu a');
-    console.log(menuList)
-    for (let list = 0; list < menuList.length; list += 1) {
-        menuList[list].addEventListener('click', () =>{
-            mainMenuList.remove()
-        });
-    }
-});
+hamburgerBtn.addEventListener('click', () => {
+  callMenu();
+  const mainMenuList = document.querySelector('.main-menu');
 
+  const menuList = document.querySelectorAll('.main-menu a');
+  console.log(menuList);
+  for (let list = 0; list < menuList.length; list += 1) {
+    menuList[list].addEventListener('click', () => {
+      mainMenuList.remove();
+    });
+  }
+});
