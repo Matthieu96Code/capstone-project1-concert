1a8dcd2fd2eeccaf038261d8489cc01fad99056d
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-02-08 10:00:09 +0000

    link some menu item to the his section

---
+++ js/menu.js
@@ -0,0 +1,33 @@
+
+const hamburgerBtn = document.querySelector('.hamburger-class a')
+
+function callMenu () {
+
+    const headerSection = document.querySelector('header')
+
+    const mainMenu = document.createElement('div');
+    mainMenu.className = 'main-menu';
+
+
+    mainMenu.innerHTML = '<a class="menu-toggle" id="close-btn" href="#" ><img src="./img/png-transparent-x-cross-wrong-sign.png" alt="close button"></a><ul><li><a href="./index.html">Home Page</a></li><li><a href="./aboutme.html">about</a></li><li><a href="./index.html#program">Program</a></li><li><a href="#join">Join</a></li><li><a href="#sponsor">Sponsor</a></li><li><a href="#news">News</a></li><li><a href="lp-compaign">LP Compaign</a></li></ul>'
+
+    headerSection.appendChild(mainMenu)
+}
+
+hamburgerBtn.addEventListener('click', () =>{
+    callMenu ()
+    const mainMenuList = document.querySelector('.main-menu');
+    /*const crossIcon = document.querySelector('.main-menu #close-btn img');
+    crossIcon.addEventListener('click', () =>{
+        mainMenuList.remove()
+    });*/
+    
+    const menuList = document.querySelectorAll('.main-menu a');
+    console.log(menuList)
+    for (let list = 0; list < menuList.length; list += 1) {
+        menuList[list].addEventListener('click', () =>{
+            mainMenuList.remove()
+        });
+    }
+});
+
